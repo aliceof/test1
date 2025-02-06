29cfb6e026d622144ce3feef37792ded36f8f437
aliceof <shuixian789@qq.com>
2025-02-06 22:56:47 +0800

    alice2-6

--- src/main/java/com/alice/test1/test1/mapper/UserMapper.java
+++ src/main/java/com/alice/test1/test1/mapper/UserMapper.java
@@ -1,5 +1,10 @@
 package com.alice.test1.test1.mapper;
 
 
-public class UserMapper {
+import com.alice.test1.test1.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<User> findByUsername();
 }
